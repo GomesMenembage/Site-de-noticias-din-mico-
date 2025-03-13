e59cd4fcc4373cca40cebbea86da80d900425533
/Coder.444 <mikeykkj15@gmail.com>
2025-03-13 22:05:24 +0100

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,5 @@
+document.getElementById('filterBtn').addEventListener('click', function() {
+    var filterOptions = document.getElementById('filterOptions');
+    filterOptions.style.display = filterOptions.style.display === 'none' ? 'block' : 'none';
+});
+
